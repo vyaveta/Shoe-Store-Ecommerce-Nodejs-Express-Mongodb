1b9a012c9b64d1b5cceb4eb3aff6e4a003e2c44c
vyaveta <adwaithkv777@gmail.com>
2022-09-18 15:16:05 +0530

    SEPT 18.2 UPDATION

---
+++ public/javascripts/customAlertBox.js
@@ -0,0 +1,29 @@
+const alert__btn = document.querySelector('.alert')
+const alertBox = document.querySelector('.alert__box')
+const close = document.querySelector('.close__alert')
+let timer;
+
+// alert__btn.addEventListener('click',(e)=>{
+//     console.log('alert button clicked')
+//    showAlertBox()
+// })
+function hideAlertBox()  {
+   alertBox.classList.remove('show')
+   alertBox.classList.add('hide')
+}
+close.addEventListener('click',(e)=>{
+    hideAlertBox()
+    clearTimeout()
+})
+function  showAlertBox(){
+console.log('im show alert box')
+    alertBox.classList.remove('hide')
+    alertBox.classList.add('show')
+    if(alertBox.classList.contains('none')){
+        alertBox.classList.remove('none')
+    }
+    //now hide the animation after 5sec
+    timer = setTimeout(()=>{
+        hideAlertBox()
+    },5000)
+}
