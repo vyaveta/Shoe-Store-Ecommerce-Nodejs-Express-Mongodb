1dfdf03c1a5475a53f475b683f0ed5033cce725d
vyaveta <adwaithkv777@gmail.com>
2022-09-26 22:33:27 +0530

    sept 26 night updation

--- public/javascripts/admin__profile.js
+++ public/javascripts/admin__profile.js
@@ -1,7 +1,11 @@
 var popBox = document.getElementById('pop__up')
 button = document.getElementById('pop__signal')
+var close = document.getElementById('close__button')
 
 button.onclick = ()=>{
     console.log('button clicked')
     popBox.style.visibility = 'visible'
+}
+close.onclick = () =>{
+    popBox.style.visibility = 'hidden'
 }
